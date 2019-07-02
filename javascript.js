95003509bb87abc7339d8e28358f056544b5d1d4
Locness <locness3@orange.fr>
2019-07-02 11:19:49 +0200

    modernize and clean JS code + use less absolute urls

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
-if (window.location.href=="https://accio1.github.io/SSE/") {
-	window.location.replace("https://accio1.github.io/SSE/home/");
-	}
+if (window.location.hash === "" || window.location.hash === "#") {
+	window.location.href = "/home";
+}
 
 var pull = new XMLHttpRequest();
-var Oldsid = location.hash
-var sid = Oldsid.substring(1)
-pull.open("GET", "https://cors-anywhere.herokuapp.com/api.scratch.mit.edu/studios/"+sid+"/");
+var Oldsid = location.hash;
+var sid = Oldsid.substring(1);
+pull.open("GET", `https://cors-anywhere.herokuapp.com/api.scratch.mit.edu/studios/${sid}`);
 pull.send();
 	pull.onreadystatechange = function() {
 		if (pull.readyState === 4 && pull.status === 200) {
 			var pulldone = JSON.parse(pull.responseText
 			);
 			var title = pulldone.title
-			document.getElementById("title").innerHTML = "<a href="+"https://scratch.mit.edu/studios/"+sid+"/>"+title+"</a>";
-			document.getElementById("image").innerHTML = "<img 		src=https://cdn2.scratch.mit.edu/get_image/gallery/"+sid+"_200x130.png class='image'>";
+			document.getElementById("title").innerHTML = `<a href="https://scratch.mit.edu/studios/${sid}">${title}</a>`;
+			document.getElementById("image").innerHTML = `<img src=https://cdn2.scratch.mit.edu/get_image/gallery/${sid}_200x130.png class='image'>`;
 			var desc = pulldone.description
 			document.getElementById("desc").innerHTML = desc;
 		}
