1d765c5f8705a4690aff805cfdc4eae2c3810bb6
-Accio- <48496940+Accio1@users.noreply.github.com>
2019-07-09 11:40:31 -0400

    Merge of: 5/7/19 functional+cosmetic changes (#3)
    Change Log:
    -Added a 404 page if the studio id is invalid.
    -Added shortcuts which can replace studio ids (Featured for the current featured studio, SDS for the current SDS).
    -Added a min-height style property which makes sure the box is always a certain size.
    -Made the default title "Loading..." so users know that it's loading.
    -Moved the JavaScript for the homepage into a separate file instead of script tags.
    -Changed <input type="submit"> to buttons to make things more consistent.
    -Added a little bubble next to the title which says "By: -Accio-"
    -Added some text which appears when someone uses the copy button

---
+++ main.js
@@ -0,0 +1,49 @@
+setTimeout(display, 50)
+
+if (window.location.hash === "" || window.location.hash === "#") {
+	window.location.replace("home");
+	} 
+	
+function error(title, message) {
+	document.getElementById("title").innerHTML = title;
+	document.getElementById("error").innerHTML = message;
+}
+		
+var pull = new XMLHttpRequest()		
+var Oldsid = location.hash
+var sid = Oldsid.substring(1).toUpperCase();
+	
+if (sid=="SDS" || sid=="FEATURED") {
+	pull.open("GET", "https://cors-anywhere.herokuapp.com/api.scratch.mit.edu/proxy/featured");
+	pull.send();
+	pull.onreadystatechange = function() {
+  		if (pull.readyState === 4 && pull.status === 200) {
+   			pulldone = JSON.parse(pull.responseText);
+			if (sid == "SDS") {
+				sid = pulldone.scratch_design_studio[0].gallery_id;
+			}
+			else if (sid == "FEATURED") {
+				sid = pulldone.community_featured_studios[0].id
+			}
+		}
+	}
+}
+	
+function display(){
+	var pull = new XMLHttpRequest()
+	pull.open("GET", `https://cors-anywhere.herokuapp.com/api.scratch.mit.edu/studios/${sid}`);
+	pull.send();
+		pull.onreadystatechange = function() {
+			if (pull.readyState === 4 && pull.status === 200) {
+				var pulldone = JSON.parse(pull.responseText);
+				var title = pulldone.title
+				document.getElementById("title").innerHTML = `<a href="https://scratch.mit.edu/studios/${sid}">${title}</a>`;
+				document.getElementById("image").innerHTML = `<img src=https://cdn2.scratch.mit.edu/get_image/gallery/${sid}_200x130.png class='image'>`;
+				var desc = pulldone.description
+				document.getElementById("textarea").innerHTML = `<textarea id="desc" readonly>${desc}</textarea>`;
+			}
+			else if (pull.status === 404) {
+    			error("Studio not found", "Make sure you have the correct Studio ID.");
+ 			}
+	}
+}
